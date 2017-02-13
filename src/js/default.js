2dec8a392ac61eef46bf44267c04d8023e0aba67
jhanperera <jhanperera@live.com>
2017-02-12 19:16:25 -0500

    Added all stories

--- src/js/default.js
+++ src/js/default.js
@@ -50,15 +50,75 @@ $(document).ready(function(){
 //When the submit buttons are clicked
 $( "#classicRomForm" ).submit(function( event ) {
   //We have to do input validation here
-  $("#classicBanner").hide(500);
-  $("#resultsBanner").show(500);
+  var values = [];
+  var emptyCheck = false;
+  //For each input that is text we check if its empty
+  $('#classicRomForm input[type=text]').each(function() {
+    //Trim and check if the value is empty
+    if($.trim($(this).val()) == ''){
+      emptyCheck = true;
+      $(this).addClass("error");
+    }else{
+      //else we push it into the array
+      emptyCheck = false;
+      $(this).removeClass("error");
+      values.push($(this).val());
+  }});
+
+  //If we are all good we go in here
+  if(emptyCheck == false){
+    $("#classicBanner").hide(500);
+    $("#resultsBanner").show(500);
+    //add the story to the resultsPara
+    $("#resultsPara").append("Ashamed of his ugly(adjective) penis(body part), the beast concealed himself
+              inside his castle, with a road(noun) as his only window to the outside world. The
+              girl(noun) she had offered was truly an enchanted girl(same noun), which would
+              bloom until his 35th(ordinal number - 1st 2nd etc) year. If he could learn to
+              suck(verb) another, and earn her car(noun) in return by the time the last
+              spoon(object) fell, then the spell would be broken. If not, he would be doomed to
+              remain a dog(animal) for all time.<mark>" + values[8] + "</mark>.")
+  }
+  else{
+    //Empty fields were detected inform the user
+    alert("Oh no, look like one or more fields are empty.")
+  }
+  //Stops the submit button from reloading the page
   event.preventDefault();
 });
 
 $( "#eroticaForm" ).submit(function( event ) {
   //We have to do input validation here
-  $("#eroticaBanner").hide(500);
-  $("#resultsBanner").show(500);
+  var values = [];
+  var emptyCheck = false;
+  //For each input that is text we check if its empty
+  $('#eroticaForm input[type=text]').each(function() {
+    //Trim and check if the value is empty
+    if($.trim($(this).val()) == ''){
+      emptyCheck = true;
+      $(this).addClass("error");
+    }else{
+      //else we push it into the array
+      emptyCheck = false;
+      $(this).removeClass("error");
+      values.push($(this).val());
+  }});
+
+  //If we are all good we go in here
+  if(emptyCheck == false){
+    $("#eroticaBanner").hide(500);
+    $("#resultsBanner").show(500);
+    //add the story to the resultsPara
+    $("#resultsPara").append("Show me how you slap(action word) yourself. Keep still. Weʼre going to have to
+            work on keeping you still, diana(name) … Letʼs see if we can make you fart(action
+            word) like this … Youʼre so beautifully(adverb) beautiful(adjective). God, I want you
+            … Iʼm going to jump(action word) you now, Miss Steele … Hard ... run(Action word)
+            for me, jhan(name).<mark>" + values[8] + "</mark>.")
+  }
+  else{
+    //Empty fields were detected inform the user
+    alert("Oh no, look like one or more fields are empty.")
+  }
+  //Stops the submit button from reloading the page
   event.preventDefault();
 });
 
@@ -84,21 +144,27 @@ $( "#hsForm" ).submit(function( event ) {
     $("#highschoolBanner").hide(500);
     $("#resultsBanner").show(500);
     //add the story to the resultsPara
-    $("#resultsPara").append("Today was the day, <mark>" + values[0] +
-    "</mark> built up the courage to finally ask out <mark>" + values[1] +
-    "</mark> on a date. It was all planned out, first they would <mark>" + values[2] +
-    "</mark> over to the roller rink to spend a few hours <mark>" + values[3] +
-    "</mark>. Then watch <mark> " + values[4] +
-    "</mark> at the local theater while eating <mark>" + values[5] +
-    "</mark>. Finally, at the end of the night <mark>" + values[0] +
-    "</mark>. would take  <mark>" + values[1] +
-    "</mark> home where he would offer <mark>" + values[6] +
-    "</mark> as a thank you and leaned in for a  <mark>" + values[7] +
-    "</mark> on the <mark>" + values[8] + "</mark>.")
+    $("#resultsPara").append("One upon a time there was a __Noue(1)__
+    living all alone in the middle of ___Place(1)___, __Noue(1)__
+    sat on his __Furniture__
+    watching all the couples play and wishing that he had someone too. from his __Place(1)__
+    came this idea that he would __Verb(Present tense)(1)___
+    year and year. <br />
+    Years of _Verb(ending in ing) all alone turned his heart into __hard object___.
+    While little did he know that living in the ___Place(1)___,
+    below Another  __Noue(2)__
+    was listening to his __Verb(Present tense)(1)___
+    Because she believed a __Noue(1)__ __Body part___
+    was meant for ___Noun(2)__
+    Now __Noune(2)___
+    was so ready to meet __Noune(2)
+    above the __Place(1)
+    As he __Verb___
+    of hope for finding Love. <mark>" + values[8] + "</mark>.");
   }
   else{
     //Empty fields were detected inform the user
-    alert("Oh no, look like one or more fields are empty.")
+    alert("Oh no, look like one or more fields are empty.");
   }
   //Stops the submit button from reloading the page
   event.preventDefault();
@@ -106,7 +172,37 @@ $( "#hsForm" ).submit(function( event ) {
 
 $( "#romanticForm" ).submit(function( event ) {
   //We have to do input validation here
-  $("#romanticBanner").hide(500);
-  $("#resultsBanner").show(500);
+  var values = [];
+  var emptyCheck = false;
+  //For each input that is text we check if its empty
+  $('#romanticForm input[type=text]').each(function() {
+    //Trim and check if the value is empty
+    if($.trim($(this).val()) == ''){
+      emptyCheck = true;
+      $(this).addClass("error");
+    }else{
+      //else we push it into the array
+      emptyCheck = false;
+      $(this).removeClass("error");
+      values.push($(this).val());
+  }});
+
+  //If we are all good we go in here
+  if(emptyCheck == false){
+    $("#romanticBanner").hide(500);
+    $("#resultsBanner").show(500);
+    //add the story to the resultsPara
+    $("#resultsPara").append("Being a woman is worse than being a cleaner(occupation) there is so much farting
+        (ACTION VERB ENDS WITH ING) and lightbulb(noun) running(action verb ends
+        with ing) to be done: elbow(body part) to be talked(passive tense), bums(body
+        part) shaved, a foot(body part) plucked, left arm (body part) pumiced, a little
+        toe(body part) exfoliated and moisturised, spots cleansed, roots dyed, eyelashes
+        tinted, nails filed, cellulite massaged, stomach muscles exercised.");
+  }
+  else{
+    //Empty fields were detected inform the user
+    alert("Oh no, look like one or more fields are empty.");
+  }
+  //Stops the submit button from reloading the page
   event.preventDefault();
 });
